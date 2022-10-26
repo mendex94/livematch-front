d2b98dbc3a8bd73efd5f04da53c1fea32e2dd5ef
mendex94 <lfelipecmendes@icloud.com>
2022-10-26 15:34:12 -0300

    feat:react-query-setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps, router }: AppProps) {
+  const queryClient = new QueryClient()
+
   return (
     <Layout router={router}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Layout>
   )
 }
